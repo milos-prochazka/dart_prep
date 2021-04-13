5f21ad3e3e5e558f9676dc9a8ad60ad7ef204bf9
prochazka@seapraha.cz <prochazka@seapraha.cz>
2021-04-13 16:33:02 +0200

    Upraveny git push

--- git-push.bat
+++ git-push.bat
@@ -1,3 +1,4 @@
+if .%1.==.. goto syntax
 dart-prep --enable-all .\
 git add --all
 git commit --all -m %1
@@ -5,3 +6,7 @@ git push --all
 git gc
 git gc --aggressive
 git prune
+goto end
+:syntax
+@echo Syntax: git-push "message"
+:end
